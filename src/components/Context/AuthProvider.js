c8fde79039ff58a1e23958363987c7e7d85d94d2
SaifullaJubair <saifullajubair6@gmail.com>
2022-11-08 16:16:01 +0600

    Auth

---
+++ src/components/Context/AuthProvider.js
@@ -0,0 +1,74 @@
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../../firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
+export const AuthContext = createContext()
+const auth = getAuth(app)
+
+
+const AuthProvider = ({ children }) => {
+
+   const [user, setUser] = useState(null);
+   const [loading, setLoading] = useState(true);
+
+   const createUser = (email, password) => {
+      setLoading(true);
+      return createUserWithEmailAndPassword(auth, email, password);
+
+   }
+   //google SignIn
+   const googleProvider = new GoogleAuthProvider()
+   const signInWithGoogle = () => {
+      return signInWithPopup(auth, googleProvider)
+   }
+
+   const githubProvider = new GithubAuthProvider()
+
+   const signInWithGithub = () => {
+      return signInWithPopup(auth, githubProvider)
+   }
+   //email login
+   const signIn = (email, password) => {
+      setLoading(true)
+      return signInWithEmailAndPassword(auth, email, password)
+   }
+
+   //verify email
+   const verifyEmail = () => {
+      return sendEmailVerification(auth.currentUser);
+   }
+
+   const updateUserProfile = (profile) => {
+      return updateProfile(auth.currentUser, profile)
+   }
+
+   const logOut = () => {
+      setLoading(true)
+      return signOut(auth)
+   }
+
+
+
+
+   useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, currentUser => {
+         console.log('user inside state change', currentUser)
+         setUser(currentUser);
+         setLoading(false)
+      })
+      return () => {
+         unsubscribe();
+      }
+   }, [])
+
+
+   const authInfo = { user, createUser, signIn, signInWithGoogle, logOut, loading, updateUserProfile, verifyEmail, signInWithGithub }
+   return (
+      <div>
+         <AuthContext.Provider value={authInfo}>
+            {children}
+         </AuthContext.Provider>
+      </div>
+   );
+};
+
+export default AuthProvider;
